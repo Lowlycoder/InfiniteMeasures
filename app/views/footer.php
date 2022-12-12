2bfe3f596b3059a9a33b388616636c4ee7f2a3ad
Hermann BONGA <bongahermannkenneth@gmail.com>
2022-12-12 10:37:53 +0100

    HomePage view

---
+++ app/views/footer.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="https://kit.fontawesome.com/bb762585be.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"> 
+</head>
+
+
+    <footer style="background-color: #88CAF0;border-top: 1px solid red;padding:10px 0;width:100%;position:relative;">
+        <div class="footer-container" style="color:white;margin: auto;max-width: 1170px;height:50px;margin-bottom: 50px;">
+            <div class="row" style="display:flex;flex-direction: row;margin-left:100px;">
+                <div class="contacts"  style="width:25%;padding: 0 15px;margin-left:20px;">
+                <h3 style="padding-left:50px">Contacts</h3>
+                    <span style="padding-left:30px">+33 0 78 54 21 69</span><br>
+                    <span>Adresse: 75011 Paris, France</span>
+                </div>
+                <div class="info"  style="width:25%;padding: 0 15px;margin-left:150px;">
+                    <a href="#" style="color:white;">CGU</a>
+                    <a href="#" style="color:white;">FAQ</a>
+                    <a href="#" style="color:white;">QCM</a>
+                </div>
+                <div class="social_network" style="width:25%;padding: 0 15px;margin-left:100px;">
+                    <a href="#" style="color:white;"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#" style="color:white;"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#" style="color:white;"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#" style="color:white;"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div style="padding-left: 15px;margin-top: 20px;">
+            <div style="color:rgb(94, 93, 93);">
+                Copyright © 2022 Infinite Measure Inc. All Right Reserved.
+            </div>
+        </div><br>
+                
+    </footer>
+   
+
+</body>
+</html>
