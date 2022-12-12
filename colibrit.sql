851d784e91749531ae9d59033a8026f96f500ea3
Hermann BONGA <bongahermannkenneth@gmail.com>
2022-12-12 10:16:34 +0100

    adding files

---
+++ colibrit.sql
@@ -0,0 +1,160 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 12, 2022 at 09:09 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `colibrit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `idAdmin` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `prenom` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `email` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `passwordHash` varchar(255) COLLATE utf8mb3_bin NOT NULL,
+  PRIMARY KEY (`idAdmin`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `boitier`
+--
+
+DROP TABLE IF EXISTS `boitier`;
+CREATE TABLE IF NOT EXISTS `boitier` (
+  `idboitier` int NOT NULL AUTO_INCREMENT,
+  `User_idUser` int NOT NULL,
+  PRIMARY KEY (`idboitier`,`User_idUser`),
+  KEY `fk_boitier_User1_idx` (`User_idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `capteur`
+--
+
+DROP TABLE IF EXISTS `capteur`;
+CREATE TABLE IF NOT EXISTS `capteur` (
+  `idcapteur` int NOT NULL AUTO_INCREMENT,
+  `minUser` int DEFAULT NULL,
+  `maxUser` int DEFAULT NULL,
+  `typeCapteur_idTypeCapteur` int NOT NULL,
+  `boitier_idboitier` int NOT NULL,
+  PRIMARY KEY (`idcapteur`,`typeCapteur_idTypeCapteur`,`boitier_idboitier`),
+  KEY `fk_capteur_typeCapteur1_idx` (`typeCapteur_idTypeCapteur`),
+  KEY `fk_capteur_boitier1_idx` (`boitier_idboitier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE IF NOT EXISTS `data` (
+  `idData` int NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `value` int NOT NULL,
+  `capteur_idcapteur` int NOT NULL,
+  PRIMARY KEY (`idData`,`capteur_idcapteur`),
+  KEY `fk_data_capteur1_idx` (`capteur_idcapteur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faq`
+--
+
+DROP TABLE IF EXISTS `faq`;
+CREATE TABLE IF NOT EXISTS `faq` (
+  `idFAQ` int NOT NULL AUTO_INCREMENT,
+  `question` varchar(255) COLLATE utf8mb3_bin NOT NULL,
+  `reponse` multilinestring NOT NULL,
+  PRIMARY KEY (`idFAQ`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `typecapteur`
+--
+
+DROP TABLE IF EXISTS `typecapteur`;
+CREATE TABLE IF NOT EXISTS `typecapteur` (
+  `idTypeCapteur` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `max` int DEFAULT NULL,
+  `min` int DEFAULT NULL,
+  PRIMARY KEY (`idTypeCapteur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `idUser` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `prenom` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `email` varchar(45) COLLATE utf8mb3_bin NOT NULL,
+  `passwordHash` varchar(255) COLLATE utf8mb3_bin NOT NULL,
+  `lastConnection` datetime NOT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `boitier`
+--
+ALTER TABLE `boitier`
+  ADD CONSTRAINT `fk_boitier_User1` FOREIGN KEY (`User_idUser`) REFERENCES `user` (`idUser`);
+
+--
+-- Constraints for table `capteur`
+--
+ALTER TABLE `capteur`
+  ADD CONSTRAINT `fk_capteur_boitier` FOREIGN KEY (`boitier_idboitier`) REFERENCES `boitier` (`idboitier`),
+  ADD CONSTRAINT `fk_capteur_typeCapteur` FOREIGN KEY (`typeCapteur_idTypeCapteur`) REFERENCES `typecapteur` (`idTypeCapteur`);
+
+--
+-- Constraints for table `data`
+--
+ALTER TABLE `data`
+  ADD CONSTRAINT `fk_data_capteur` FOREIGN KEY (`capteur_idcapteur`) REFERENCES `capteur` (`idcapteur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
