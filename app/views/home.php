6c276e412a1d8b1226a1405339a80addef60c181
Hermann BONGA <bongahermannkenneth@gmail.com>
2022-12-15 15:55:16 +0100

    Supression de la première page d'acceuille

--- app/views/home.php
+++ app/views/home.php
@@ -1,28 +1,7 @@
 <?php
 require("header.php");
 ?>
-<div class="site-container">
-    <img src="<?= BASE_URL_ASSETS ?>images/logo50x50.png" alt="logo"/>
-    <h1>
-        <!-- title from model -->
-        <?= $this->escapeHtml($page['info']['title']) ?>
-    </h1>
-    <div class="section-container">
-        <h3>
-            <!-- subtitle from model -->
-            <?= $this->escapeHtml($page['info']['subtitle']) ?>
-        </h3>
-    </div>
-    <!-- description html snippet -->
-    <?= $description_html ?>
+<img src="<?= BASE_URL_ASSETS ?>images/home_picture.jpg" alt="home_picture" width="100%" height="100%">
 
-    <?php foreach ($page['links'] as $link): ?>
-        <div class="section-container">
-            <a href="<?= $this->escapeHtml($link['link_url']) ?>" target="_blank">
-                <?= $this->escapeHtml($link['link_title']) ?>
-            </a>
-        </div>
-    <?php endforeach; ?>
-</div>
 <?php
 require('footer.php');
