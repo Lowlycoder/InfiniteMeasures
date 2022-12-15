2aa4fecb868b7cb693ffd50f43b8e7fd1d0fb44e
Hermann BONGA <bongahermannkenneth@gmail.com>
2022-12-15 17:36:04 +0100

    Connection de la page contact au site

--- app/views/contact.php
+++ app/views/contact.php
@@ -14,7 +14,7 @@
 
         <div class="contact_form">
 
-            <form action="contact.php" method="POST">
+            <form action="/contact" method="POST">
                 <label for="name" >Nom et Prénom</label><br>
                 <input type="text" name="name" id="name" class="input-field"><br>
                 <label for="adresse_mail">Email</label><br>
@@ -29,35 +29,12 @@
         </div>
 
         <div class="logo">
-            <img src="./images/logo_Isanté.png" alt="logo" >
+            <img src="<?= BASE_URL_ASSETS ?>images/logo_Isanté.png" alt="logo" >
         </div>
 
     </div>
     
-    <?php
 
-if (isset($_POST['subject'])) // On vérifie si le sujet a été remplie: c'est juste pour éviter que le serveur exécute tout le script php. Ce qui enverait une erreure
-{
-    $to="isante044@gmail.com";   //destinataire du mail
-    $subject=$_POST['subject'];  //sujet du mail
-    $message="<strong>From :</strong>{$_POST['email']}<br>{$_POST['message']}<br><br><br>Cordialement ".$_POST['name'].'.'; //message du mail
-
-    $headers=array(
-        'From'=>  $_POST['email'],
-        'Reply-To' => $_POST['email'],
-        'content-type' => 'text/html'
-    );   // Le header est un paramètre de la fonction mail qui contient entêtes
-   
-    $sentMail=mail($to,$subject,$message,$headers);
-    if(!empty($_POST['message']) ){
-        if($sentMail){
-            echo "Your email has been sent successfully !";
-        }else{
-            echo "Error !!!";
-        }
-    }
-}
-    ?>
 
 
 
