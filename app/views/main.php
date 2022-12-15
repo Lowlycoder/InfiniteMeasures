3fd669e03755bb41775afd6decd6740221794e11
Hermann BONGA <bongahermannkenneth@gmail.com>
2022-12-15 15:56:13 +0100

    Ajout du lien de la page style contact

--- app/views/main.php
+++ app/views/main.php
@@ -19,6 +19,7 @@
           <link href="<?= BASE_URL_ASSETS ?>css/mobile.css" media="screen and (max-width: 1400px)"
                 rel="stylesheet" type="text/css">
           <link href="<?= BASE_URL_ASSETS ?>css/style.css" rel="stylesheet" type="text/css">
+          <link href="<?= BASE_URL_ASSETS ?>css/contact.css" rel="stylesheet" type="text/css">
           
       <script src="https://kit.fontawesome.com/bb762585be.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"> 
