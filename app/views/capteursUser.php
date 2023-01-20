682f7a984a7157986ca2929d56aaec1084c749f4
Gregory Bonneto <gbonneto@gmail.com>
2023-01-20 17:23:16 +0100

    adding user page

---
+++ app/views/capteursUser.php
@@ -0,0 +1,38 @@
+<div class="grid-container">
+
+
+
+
+    <!--début de la sidebar-->
+    <aside id="sidebar">
+        <div class="sidebar-title">
+            <div class="sidebar-brand">
+                <span class="material-icons-outlined">home</span> Bonjour, john jones
+            </div>
+            <span class="material-icons-outlined" onclick="closeSidebar()">close</span>
+        </div>
+        <ul class="sidebar-list">
+            <li class="sidebar-list-item">
+                <a href="/dashboard"><span class="material-icons-outlined">dashboard</span>Tableau de Bord</a>
+            </li>
+            <li class="sidebar-list-item">
+                <a href="/capteursUser"><span class="material-icons-outlined">cable</span>Capteurs</a>
+            </li>
+            <li class="sidebar-list-item">
+                <a href="/Réglages"><span class="material-icons-outlined">settings</span>Réglages</a>
+            </li>
+        </ul>
+    </aside>
+    <!--fin de la sidebar-->
+
+
+
+
+    <!--début du milieu de page-->
+    <main class="main-container">
+
+
+
+
+    </main>
+</div>
