bea492f61cc45507e389afce4e85e4ba2afc6246
Cassandre Arminjon <cassandre.arminjon@gmail.com>
2023-01-25 19:32:28 +0100

    ajout du carousel de page d'accueil

--- app/views/home.php
+++ app/views/home.php
@@ -1,1 +1,45 @@
-<img src="<?= BASE_URL_ASSETS; ?>images/sick-boy.jpg" alt="home_picture" width="100%" height="100%">
+<div class="container">
+    <div class="swiper">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+              <!-- Slides -->
+              <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image1.jpg"></div>
+              <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image2.jpg"></div>
+              <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image3.jpg"></div>
+              <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image4.jpg"></div>
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+          
+            <!-- If we need navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+          
+    </div>
+</div>
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<script>
+    const swiper = new Swiper('.swiper', {
+        autoplay:{
+            delay:3000,
+            disableOnInteraction:false,
+
+        },
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, 
+    },
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+ 
+});
+
+</script>
