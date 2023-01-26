ffafc3a82646e22b30b060259f2878719fbd4a0a
Benjamin Bordes <benjaminbordes78@gmail.com>
2023-01-26 02:06:42 +0100

    fix faq

--- app/views/faq.php
+++ app/views/faq.php
@@ -1,6 +1,7 @@
 <div class="container" style="margin:3rem auto;">
     <h3 style="margin-bottom: 1rem">FAQ</h3>
     <div class="accordion">
+        <?= isset($faq) ? null : 'Aucune question n\'est disponible à l\'affichage'; ?>
         <?php foreach (($faq ?? null) as $question): ?>
             <div class="accordion-item">
                 <input type="checkbox" id="<?= $this->escapeHtml($question['idFAQ']); ?>" />
@@ -12,30 +13,5 @@
                 </div>
             </div>
         <?php endforeach; ?>
-        <div class="accordion-item">
-            <input type="checkbox" id="faq2" />
-            <label for="faq2">
-                Accordion Item #2
-            </label>
-            <div class="accordion-body">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-        </div>
-        <div class="accordion-item">
-            <input type="checkbox" id="faq3" />
-            <label for="faq3">
-                Accordion Item #3
-            </label>
-            <div class="accordion-body">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-        </div>
-        <div class="accordion-item">
-            <input type="checkbox" id="faq4" />
-            <label for="faq4">
-                Accordion Item #4
-            </label>
-            <div class="accordion-body">this is some random text</div>
-        </div>
     </div>
 </div>
