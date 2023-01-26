265ce3df0b1cd306b056d12ee7ce65060aa8f54f
Benjamin Bordes <benjaminbordes78@gmail.com>
2023-01-26 02:25:06 +0100

    fix cs

--- app/views/home.php
+++ app/views/home.php
@@ -1,45 +1,45 @@
 <div class="container">
     <div class="swiper">
-            <!-- Additional required wrapper -->
-            <div class="swiper-wrapper">
-              <!-- Slides -->
-              <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image1.jpg"></div>
-              <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image2.jpg"></div>
-              <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image3.jpg"></div>
-              <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image4.jpg"></div>
-            </div>
-            <!-- If we need pagination -->
-            <div class="swiper-pagination"></div>
-          
-            <!-- If we need navigation buttons -->
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-          
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image1.jpg" alt="Sick boy"></div>
+            <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image2.jpg" alt="Medications"></div>
+            <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image3.jpg" alt="Running"></div>
+            <div class="swiper-slide"><img src="<?= BASE_URL_ASSETS; ?>images/image4.jpg" alt="Colibri"></div>
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
+
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
     </div>
 </div>
 
 <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 
 <script>
-    const swiper = new Swiper('.swiper', {
-        autoplay:{
-            delay:3000,
-            disableOnInteraction:false,
+    const swiper = new Swiper(".swiper", {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
 
         },
-    loop: true,
+        loop: true,
 
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true, 
-    },
-    
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
 
- 
-});
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+
+    });
 
 </script>
